165454879784c1c7f5e9adc36ca25be3cf8f1841
jourialem123 <alemjouri@gmail.com>
2025-01-15 19:06:16 +0300

    project38.....2

---
+++ project38.py
@@ -0,0 +1,17 @@
+x=5
+y=9
+z=1
+print("The value of x before swapping is ",x)
+print("The value of y before swapping is ",y)
+print("The value of z before swapping is ",z)
+
+temp=x
+x=z
+z=y
+y=temp
+
+
+
+print("The value of x after swapping is ",x)
+print("The value of y after swapping is ",y)
+print("The value of z after swapping is ",z)
